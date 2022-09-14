ca133234395a4894e66205cd244cc19614925124
杨文军 <juyiyang@qq.com>
2022-09-14 21:18:24 +0800

    socket

---
+++ pages/chat/chat.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="">
+		socket
+	</view>
+</template>
+
+<script setup>
+const socket = 	wx.connectSocket({
+		url: 'ws://192.168.31.247:3001',
+		header: {
+			'content-type': 'application/json'
+		},
+	})
+	// 监听
+	wx.onSocketOpen((result) => {
+		wx.sendSocketMessage({
+			data: {
+				type:'JSON',
+				data:{
+					a:12134
+				}
+			}
+		})
+	})
+	// 失败监控
+	wx.onSocketError((result) => {
+		console.log("websocket连接失败", result);
+	})
+	// 监听服务器的数据返回
+	wx.onSocketMessage((result) => {
+		console.log("服务器的数据返回", result);
+	})
+</script>
+
+<style lang="less" scoped>
+</style>
